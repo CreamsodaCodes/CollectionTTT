be1cb9abe37dfe9e5e6374b661d1b80d32c81953
CreamsodaCodes <noahnsteiner@googlemail.com>
2019-11-13 17:12:21 +0100

    Erster

---
+++ TTT 2d Nr1/Assets/Scripts/Shotgun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shotgun : MonoBehaviour
+{
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+       Debug.Log(other);
+       GameObject player = other.gameObject;
+       player.GetComponent<Shooting>().enabled = true;
+         
+
+    }
+}
---
+++ TTT 2d Nr1/Assets/Scripts/Sniper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : MonoBehaviour
+{
+    public GameObject Player;
+ void Update ()
+ {
+     if (Input.GetButtonDown("Fire2"))
+     {
+         Shoot();
+     }
+ }
+ void Shoot () {        
+         Vector3 startPos = Player.transform.position;
+         Vector3 endPos = Player.transform.up * 10;    
+         Debug.DrawRay (startPos, endPos);
+         RaycastHit hit;
+         if (Physics.Raycast (startPos, endPos, out hit)) {
+             Debug.Log (hit.transform.name + " was hit");
+         }
+     }
+}
