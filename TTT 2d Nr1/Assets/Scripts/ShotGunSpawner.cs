0c156a004a6f7d9eab9909ed75cd6c184802348c
CreamsodaCodes <noahnsteiner@googlemail.com>
2019-11-14 14:42:47 +0100

    New Version
    Reparatur

--- TTT 2d Nr1/Assets/Scripts/ShotGunSpawner.cs
+++ TTT 2d Nr1/Assets/Scripts/ShotGunSpawner.cs
@@ -6,7 +6,11 @@ using UnityEngine.Networking;
 public class ShotGunSpawner : NetworkBehaviour
 {
    public GameObject shotgunPrefab;
+    public GameObject laserPrefab;
+
    public int NumberOfShotguns;
+    public int NumberOfLaser;
+
    
    public override void OnStartServer()
    {
@@ -17,6 +21,14 @@ public class ShotGunSpawner : NetworkBehaviour
 
            GameObject shotgun = (GameObject) Instantiate(shotgunPrefab, spawnPosition, spawnRotation);
            NetworkServer.Spawn(shotgun);
+       }
+       for(int i = 0; i < NumberOfShotguns; i++)
+       {
+           Vector2 spawnPosition = new Vector2(Random.Range(-8.0f,8.0f),Random.Range(-8.0f,8.0f));
+           Quaternion spawnRotation = Quaternion.Euler(0.0f,Random.Range(0.0f,180.0f),0 );
+
+           GameObject Laser = (GameObject) Instantiate(laserPrefab, spawnPosition, spawnRotation);
+           NetworkServer.Spawn(Laser);
        }
    } 
 }
